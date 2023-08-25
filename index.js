e4ad1419644e59eb599d86f1ffce20220c00d010
saint <75179785+saintycode@users.noreply.github.com>
2023-08-25 11:53:06 +0100

    Create index.js
    if statement

---
+++ index.js
@@ -0,0 +1,14 @@
+// Declare a variable and assign a value
+var x = 10;
+
+// Use an if else statement to check the value of x
+if (x > 0) {
+  // Print a message if x is positive
+  console.log("x is positive");
+} else if (x < 0) {
+  // Print a message if x is negative
+  console.log("x is negative");
+} else {
+  // Print a message if x is zero
+  console.log("x is zero");
+}
